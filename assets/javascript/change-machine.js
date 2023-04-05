17f349f977ca590734ddd538b82fc119eb707a5c
cw-roy <curtis_roy@msn.com>
2023-04-05 14:25:17 -0700

    Add basics for change, coin, and palindrome

--- assets/javascript/change-machine.js
+++ assets/javascript/change-machine.js
@@ -1,9 +1,36 @@
-(function(){
-
-    //outputs string to the body of the document
-    document.write("hello from change-machine.js");
-
-    //outputs the string to the browsers console
-    console.log("hello from change-machine.js");
-
-})();
+function calculateChange(amount) {
+    let twenties = Math.floor(amount / 20);
+    amount = amount % 20;
+    let tens = Math.floor(amount / 10);
+    amount = amount % 10;
+    let fives = Math.floor(amount / 5);
+    amount = amount % 5;
+    let ones = amount;
+  
+    let result = "<table><tr><th>Denomination</th><th>Quantity</th></tr>";
+  
+    if (twenties > 0) {
+      result += `<tr><td>Twenties</td><td>${twenties}</td></tr>`;
+    }
+    if (tens > 0) {
+      result += `<tr><td>Tens</td><td>${tens}</td></tr>`;
+    }
+    if (fives > 0) {
+      result += `<tr><td>Fives</td><td>${fives}</td></tr>`;
+    }
+    if (ones > 0) {
+      result += `<tr><td>Ones</td><td>${ones}</td></tr>`;
+    }
+  
+    result += "</table>";
+    return result;
+  }
+  
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const amount = parseInt(document.querySelector('#amount').value);
+    const result = calculateChange(amount);
+    document.getElementById("result").innerHTML = result;
+  });
+  
--- assets/javascript/coin-flip.js
+++ assets/javascript/coin-flip.js
@@ -1,9 +1,15 @@
-(function(){
-
-    //outputs string to the body of the document
-    document.write("hello from coin-flip.js");
-
-    //outputs the string to the browsers console
-    console.log("hello from coin-flip.js");
-
-})();
+function flipCoin() {
+    let randomNum = Math.random();
+  
+    if (randomNum < 0.5) {
+      return "It's heads.";
+    } else {
+      return "It's tails.";
+    }
+  }
+  
+  function showResult() {
+    let result = flipCoin();
+    document.getElementById("result").innerHTML = result;
+  }
+  
