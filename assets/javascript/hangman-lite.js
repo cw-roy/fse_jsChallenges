677f76266211b88f982d220de004ea70a2063778
cw-roy <curtis_roy@msn.com>
2023-04-05 08:31:13 -0700

    Update .gitignore, html.  Add css.

--- assets/javascript/hangman-lite.js
+++ assets/javascript/hangman-lite.js
@@ -1,5 +1,9 @@
 (function(){
-
+    
+    //outputs string to the body of the document
     document.write("hello from hangman-lite.js");
+
+    //outputs the string to the browsers console
+    console.log("hello from hangman-lite.js");
 
 })();
--- assets/javascript/tallest-mountain.js
+++ assets/javascript/tallest-mountain.js
@@ -1,5 +1,9 @@
 (function(){
-
+    
+    //outputs string to the body of the document
     document.write("hello from tallest-mountain.js");
+
+    //outputs the string to the browsers console
+    console.log("hello from tallest-mountain.js");
 
 })();
