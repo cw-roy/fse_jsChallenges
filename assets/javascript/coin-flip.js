ceac078e958546b5a9c223fb1d992c2cee93b169
cw-roy <curtis_roy@msn.com>
2023-04-08 06:50:14 -0700

    Update displayed result in coin-flip.js

--- assets/javascript/coin-flip.js
+++ assets/javascript/coin-flip.js
@@ -1,18 +1,17 @@
 function flipCoin() {
-    let randomNum = Math.random();
-  
-    if (randomNum < 0.5) {
-      return "It's heads.";
-    } else {
-      return "It's tails.";
-    }
+  let randomNum = Math.random();
+
+  if (randomNum < 0.5) {
+    return "It's heads.";
+  } else {
+    return "It's tails.";
   }
-  
-  function showResult() {
-    let result = flipCoin();
-    document.getElementById("result").innerHTML = result;
-    setTimeout(function() {
-      document.getElementById("result").innerHTML = "";
-    }, 1000); // 1000 milliseconds = 1 second
-  }
-  
+}
+
+function showResult() {
+  let result = flipCoin();
+  document.getElementById("result").innerHTML = result;
+  setTimeout(function () {
+    document.getElementById("result").innerHTML = "";
+  }, 1000);
+}
